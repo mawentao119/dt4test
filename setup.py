63f618edc0eb71c5fe8afbaff21dea74d640aed3
mawentao <mawentao119@gmail.com>
2022-02-23 09:49:30 +0800

    update version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="dt4test",
-    version="0.1.0",
+    version="0.1.1",
     description="Distributed system test library",
     long_description=long_description,
     long_description_content_type="text/markdown",
